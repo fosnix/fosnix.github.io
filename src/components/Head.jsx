99159558de611bf857143fe7f6a6f7108616f317
Uday Tiwari <udaytiwari360@outlook.com>
2024-03-06 21:06:29 +0530

    Minor Updates

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -5,12 +5,11 @@ const Head = ({ email }) => {
     return (
         <nav className="flex flex-col flex-wrap justify-between px-1 py-1 top-0 bg-stone-900 w-full content-center last:shadow-lg">
             <div className="flex flex-wrap justify-between">
-                <h2 className="text-gray-700 font-press-start text-[15px] subpixel-antialiased">Uday Tiwari <a href={"mailto:" + email} className="font-press-start text-sm hover:drop-shadow-glowOrange">[{email}]</a></h2>
+                <h2 className="text-gray-700 font-press-start text-[15px] subpixel-antialiased"><a href={"mailto:" + email} className="font-press-start text-sm hover:drop-shadow-glowOrange">Uday Tiwari</a></h2>
             </div>
-
             <div className="flex font-press-start justify-evenly text-[15px] subpixel-antialiased text-yellow-700 list-none bg-stone-700">
                 <li><a href="#">About</a></li>
-                <li><a href="#">Blogs</a></li>
+                <li><a href="#">Weblogs</a></li>
                 <li><a href="#">Projects</a></li>
             </div>
         </nav>
